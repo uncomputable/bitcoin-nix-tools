9afcbca4fef179692517898cb54a9cad74a763ce
Christian Lewe <ch_lewe@agdsn.me>
2023-10-17 13:53:10 +0200

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,19 @@
+{ nixpkgs ? import <nixpkgs> {}
+, env ? "stdenv"
+, withGui ? false
+, withWallet ? true
+, withTests ? true
+, doCheck ? true
+}:
+{
+  bitcoin = with nixpkgs; callPackage ./bitcoin.nix {
+    stdenv = nixpkgs.${env};
+    qtbase = qt5.qtbase;
+    qttools = qt5.qttools;
+    wrapQtAppsHook = qt5.wrapQtAppsHook;
+    autoSignDarwinBinariesHook = darwin.autoSignDarwinBinariesHook;
+    python3 = python3.withPackages (p: with p; [
+      pyzmq
+    ]);
+  };
+}
