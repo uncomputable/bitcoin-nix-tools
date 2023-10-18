f2a6332eec889fc63c58a7bf586e1791ede26161
Christian Lewe <ch_lewe@agdsn.me>
2023-10-18 21:42:14 +0200

    Nix: Use inherit

--- default.nix
+++ default.nix
@@ -10,10 +10,8 @@
 {
   bitcoin = with nixpkgs; callPackage ./bitcoin.nix {
     stdenv = clang14Stdenv;
-    qtbase = qt5.qtbase;
-    qttools = qt5.qttools;
-    wrapQtAppsHook = qt5.wrapQtAppsHook;
-    autoSignDarwinBinariesHook = darwin.autoSignDarwinBinariesHook;
+    inherit (qt5) qtbase qttools wrapQtAppsHook;
+    inherit (darwin) autoSignDarwinBinariesHook;
     python3 = python3.withPackages (p: with p; [
       pyzmq
     ]);
