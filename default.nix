356842b7b0b94534f06f3f50bef4a5a8fbf2587a
Christian Lewe <ch_lewe@agdsn.me>
2023-10-18 21:40:28 +0200

    Nix: Pin nixpkgs
    Use the latest stable release as a fallback while still enabling an
    override to use a custom version.
    https://github.com/NixOS/nixpkgs/commit/4ecab3273592f27479a583fb6d975d4aba3486fe

--- default.nix
+++ default.nix
@@ -1,4 +1,7 @@
-{ nixpkgs ? import <nixpkgs> {}
+{ nixpkgs ? import builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/4ecab3273592f27479a583fb6d975d4aba3486fe.tar.gz";
+    sha256 = "10wn0l08j9lgqcw8177nh2ljrnxdrpri7bp0g7nvrsn9rkawvlbf";
+  } {}
 , withGui ? false
 , withWallet ? true
 , withTests ? true
