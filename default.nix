87e48962196d5182ca43f1b842c8be69be865bc3
ekzyis <ramdip.singhgill@gmail.com>
2026-01-29 19:50:44 +0100

    Use nixos-25.11 to update to clang19Stdenv

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { nixpkgs ? import builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/057f9aecfb71c4437d2b27d3323df7f93c010b7e.tar.gz";
-    sha256 = "1ndiv385w1qyb3b18vw13991fzb9wg4cl21wglk89grsfsnra41k";
+    url = "https://github.com/NixOS/nixpkgs/archive/871b9fd269ff6246794583ce4ee1031e1da71895.tar.gz";
+    sha256 = "1zn1lsafn62sz6azx6j735fh4vwwghj8cc9x91g5sx2nrg23ap9k";
   } {}
 , withGui ? false
 , withWallet ? true
@@ -9,7 +9,7 @@
 }:
 {
   bitcoin = with nixpkgs; callPackage ./bitcoin.nix {
-    stdenv = clang16Stdenv;
+    stdenv = clang19Stdenv;
     inherit (qt5) qtbase qttools wrapQtAppsHook;
     inherit (darwin) autoSignDarwinBinariesHook;
     python3 = python3.withPackages (p: with p; [
